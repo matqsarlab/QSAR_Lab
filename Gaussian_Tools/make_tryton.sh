b3ef23385321057975863f71a2bad2abf1f45451
matqsarlab <m.balicki@qsarlab.com>
2022-07-01 16:50:48 +0200

    Update make_tryton.sh

--- Gaussian_Tools/make_tryton.sh
+++ Gaussian_Tools/make_tryton.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 DIR=$(ls -d */)
+NM=$(pwd)
 
 for i in $DIR; do
   if [[ ! -f "$i/tryton.sh" ]]; then
     echo $i"tryton.sh" is writting:
     cp tryton.sh $i
-    sed -i "" "s/ARG/${i%/*}/g" $i"tryton.sh"
+    sed -i "" "s/#SBATCH --job-name/#SBATCH --job-name  ${i%/*}_${NM##*/}/g" $i"tryton.sh"
+    sed -i "" "s/#SBATCH --output/#SBATCH --output    ${i%/*}_${NM##*/}/g" $i"tryton.sh"
+    sed -i "" "s/#SBATCH --error/#SBATCH --error     ${i%/*}_${NM##*/}/g" $i"tryton.sh"
+    sed -i "" "$ s/g16/g16  ${i%/*}_${NM##*/}.com > ${i%/*}_${NM##*/}.log/g" $i"tryton.sh"
+
   else
     echo tryton.sh file exists in $i directory.
   fi
