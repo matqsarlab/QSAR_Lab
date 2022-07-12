fa98722c463a46d044efb93101d452160272c7bc
matqsarlab <m.balicki@qsarlab.com>
2022-07-12 12:35:27 +0200

    Create 3D tools for rotation in 3D space --ver2

--- 3D_tools/quaternions_align_vectors.py
+++ 3D_tools/quaternions_align_vectors.py
@@ -31,18 +31,3 @@ def align_vectors(u, v):
         return q3
 
     return mult_quat(q, mult_quat(u, q_))[1:] * norm_u
-
-
-def rotation_matrix_from_vectors(u, v):
-    """Find the rotation matrix that aligns u to v
-    :param u: A 3d "source" vector
-    :param v: A 3d "destination" vector
-    :return mat: A transform matrix (3x3) which when applied to u, aligns it with v.
-    """
-    a, b = (u / np.linalg.norm(u)).reshape(3), (v / np.linalg.norm(v)).reshape(3)
-    v = np.cross(a, b)
-    c = np.dot(a, b)
-    s = np.linalg.norm(v)
-    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    rotation_matrix = np.eye(3) + kmat + kmat @ kmat * ((1 - c) / (s**2))
-    return rotation_matrix
