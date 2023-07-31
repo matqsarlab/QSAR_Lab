588384d571f14ebd74d6fc6654368481ff019e20
matqsarlab <m.balicki@qsarlab.com>
2023-07-31 13:30:13 +0200

    Update Ellipse function

--- Geometry/Ellipse.py
+++ Geometry/Ellipse.py
@@ -2,12 +2,14 @@ import numpy as np
 
 
 def ellipse(xy: tuple, a, b, rotate_angle):
+    """xy - center
+    a,b - diagonal of the ellipse
+    rotate_angle - rotation angle in degree
+    """
     theta = np.arange(0, 2 * np.pi, 0.01)
 
     xpos = a * np.cos(theta) + xy[0]
     ypos = b * np.sin(theta + xy[1])
-
-    rotate_angle = 90
 
     new_xpos = xpos * np.cos(np.deg2rad(rotate_angle)) + ypos * np.sin(
         np.deg2rad(rotate_angle)
